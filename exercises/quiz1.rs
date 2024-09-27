61fb03307dfa6cc3fdc1fe76dd1f98a5a7bcbed9
ekkure <142782409+ekkure@users.noreply.github.com>
2024-09-27 22:01:26 +0800

    Update quiz1.rs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -14,17 +14,6 @@
 // No hints this time ;)
 
 
-// Put your function here!
-fn calculate_price_of_apples(number: i32) -> i32 {
-
-    if number > 40 {
-        number
-    } else {
-        number * 2
-    }
-}
-
-// Don't modify this function!
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
@@ -36,4 +25,12 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(num : i32) -> i32{
+    if num>40 {
+        num 
+    }else {
+        num*2
+    }
 }
