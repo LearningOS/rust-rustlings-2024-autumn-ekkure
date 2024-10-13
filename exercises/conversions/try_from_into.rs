94d7f160fa57544d6f5f2a3131f949ae35bfcd5f
ekkure <ekkure@qq.com>
2024-10-14 02:16:17 +0800

    to 96

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,7 +27,7 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
+
 
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
@@ -40,7 +40,17 @@ enum IntoColorError {
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, IntoColorError> {
+        let (a, b, c) = tuple;
+        if (0..=255).contains(&a) && (0..=255).contains(&b) && (0..=255).contains(&c) {
+            Ok(Color {
+                red: a as u8,
+                green: b as u8,
+                blue: c as u8,
+            })
+        } else {
+            Err(IntoColorError::IntConversion)
+        }
     }
 }
 
@@ -48,6 +58,18 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let a = arr[0];
+        let b = arr[1];
+        let c = arr[2];
+        if (0..=255).contains(&a) && (0..=255).contains(&b) && (0..=255).contains(&c) {
+            Ok(Color {
+                red: a as u8,
+                green: b as u8,
+                blue: c as u8,
+            })
+        } else {
+            Err(IntoColorError::IntConversion)
+        }
     }
 }
 
@@ -55,9 +77,24 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            Err(IntoColorError::BadLen)
+        } else {
+            let a = slice[0];
+            let b = slice[1];
+            let c = slice[2];
+            if (0..=255).contains(&a) && (0..=255).contains(&b) && (0..=255).contains(&c) {
+                Ok(Color {
+                    red: a as u8,
+                    green: b as u8,
+                    blue: c as u8,
+                })
+            } else {
+                Err(IntoColorError::IntConversion)
+            }
+        }
     }
 }
-
 fn main() {
     // Use the `try_from` function
     let c1 = Color::try_from((183, 65, 14));
